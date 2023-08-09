735109179e6f5073da575c5a52c970e38c6c5b1d
Iamneo <techadmin@iamneo.ai>
2023-08-09 17:17:17 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
+interface Food {
+  name: string;
+  calories: number;
+  image: string;
+  quantity?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calorytracker';
+  pattern: string;
+  newFoodName: string;
+  newFoodCalories: number;
+  newFoodImage: string;
+  isEditing: boolean = false;
+  foods: Food[] = [];
+  myList: Food[] = [];
+
+  enableUserToAddFood() {
+    this.isEditing = true;
+  }
+
+  newFood() {
+    const newFood: Food = {
+      name: this.newFoodName,
+      calories: this.newFoodCalories,
+      image: this.newFoodImage
+    };
+
+    this.foods.push(newFood);
+    this.newFoodName = '';
+    this.newFoodCalories = null;
+    this.newFoodImage = '';
+    this.isEditing = false;
+  }
+
+  addToMyList(food: Food, quantityInput: HTMLInputElement) {
+    const quantity = parseInt(quantityInput.value);
+    const foodWithQuantity: Food = { ...food, quantity };
+    this.myList.push(foodWithQuantity);
+    quantityInput.value = '1';
+  }
+
+  calculateTotalCalories() {
+    let totalCalories = 0;
+    for (const food of this.myList) {
+      totalCalories += food.calories * (food.quantity || 1);
+    }
+    return totalCalories;
+  }
 }
